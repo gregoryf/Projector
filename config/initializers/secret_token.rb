ee3f78215ec13b94add2d0dfa5ab0591f8e1f3a7
Greg <gregorynfisher@gmail.com>
2012-02-05 15:55:11 -0300

    first setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projector::Application.config.secret_token = '0f9a4e48abf20530365f1a0e7cb707cb358a06e10499eab69ee3e1d07775d32af0575e409ccb90d4104565ddba7afb36c119397fe6cf63220cb0f626db4de280'
